9f076437f4cf387a2a2f23c541c839a48bbfa787
VinceYoon <vince.yoon01@gmail.com>
2021-08-12 11:36:21 -0700

    Apply PR suggestion

--- app/graphql/types/users/user_input.rb
+++ app/graphql/types/users/user_input.rb
@@ -1,7 +1,6 @@
 module Types::Users
   class UserInput < Types::BaseInputObject
     argument :email, String, required: true
-    argument :password, String, required: true
     argument :first_name, String, required: true
     argument :last_name, String, required: true
     argument :gender, String, required: false
